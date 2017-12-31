04e39b1ce1ea2f7e438f975b3842c3fdb9254f8a
mszuter <mszuter@gmail.com>
2017-12-31 09:43:35 -0600

    Figured out how to correctly query using pandas, added a lm using statsmodels.api. Data has been modified slightly (only Lterl Brnches) because python doesn't like variable names with spaces.

--- seaborn_trial.py
+++ seaborn_trial.py
@@ -10,13 +10,25 @@ import sys
 import math
 #from ggplot import *
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
 
 plt.style.use('ggplot')
 
 input_file = sys.argv[1]
 
 data_frame = pd.read_csv(input_file)
-plot_data = data_frame.query('Height == Height')
-sns.distplot(plot_data['Height'], bins = 20, kde = False,\
-             rug=True, label="Histogram w/o Density")
+plot_data = data_frame.query(('Height == Height & Lterl_Brnches == Lterl_Brnches'))
+#sns.distplot(plot_data['Height'], bins = 20, kde = False,\
+#             rug=True, label="Histogram w/o Density")
+#plt.show()
+
+#sns.factorplot(x='Weeding', y='Height',hue='Fence',\
+#               data=plot_data, kind='box')
+#plt.legend(loc="best")
+print(plot_data)
+sns.lmplot(x='Height', y='Lterl_Brnches', data=plot_data)
+y=plot_data['Lterl_Brnches']
+X=sm.add_constant(plot_data['Height'])
+lm = sm.OLS(y, X).fit()
+print(lm.summary())
 plt.show()
