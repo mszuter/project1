a595cecc14bb177743f6ea19046903d88abfc451
mszuter <mszuter@gmail.com>
2017-12-31 16:52:35 -0600

    Modified one remaining column in csv file, created my first html and javascript files, added an inverted figure for use in the html file.

---
+++ first_trial.js
@@ -0,0 +1,5 @@
+var cTemp = 10; //Temperature in celsius
+//Generous use of parentheses
+var hTemp = ((cTemp * 9) / 5) + 32;
+document.write("Temperature in celsius: " + cTemp + " degrees</br>");
+document.write("Temperature in Fahrenheit: " + hTemp + " degrees</br>");
