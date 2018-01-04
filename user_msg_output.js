35a53ff837ee3c55bf97af2198a5c68aed5a98c0
mszuter <mszuter@gmail.com>
2018-01-04 12:05:45 -0600

    Added several files to test out different html and javascript functions

---
+++ user_msg_output.js
@@ -0,0 +1,10 @@
+function buttonReport(buttonID, buttonName, buttonValue) {
+	//information about the button ID
+	var userMessage1 = "Button ID: " + buttonID + '\n';
+	//then about the button name
+	var userMessage2 = "Button name: " + buttonName + '\n';
+	//then about the button value
+	var userMessage3 = "Button value: " + buttonValue + '\n';
+	//alert the user
+	alert(userMessage1 + userMessage2 + userMessage3)
+}
