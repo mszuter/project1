d54d9bb9cf16ae61747cf24f1b7a3c610e4c6f46
mszuter <mszuter@gmail.com>
2018-01-05 15:17:51 -0600

    Fixed 'lateral branches', appended the poisson predictions to the dataframe, added a linear model plot of the predictions vs. Height.

--- seaborn_trial.py
+++ seaborn_trial.py
@@ -18,7 +18,7 @@ plt.style.use('ggplot')
 input_file = sys.argv[1]
 
 data_frame = pd.read_csv(input_file)
-plot_data = data_frame.query(('Height == Height & Lterl_Brnches == Lterl_Brnches'))
+plot_data = data_frame.query(('Height == Height & Lateral_Branches == Lateral_Branches'))
 #sns.distplot(plot_data['Height'], bins = 20, kde = False,\
 #             rug=True, label="Histogram w/o Density")
 #plt.show()
@@ -27,16 +27,23 @@ plot_data = data_frame.query(('Height == Height & Lterl_Brnches == Lterl_Brnches
 #               data=plot_data, kind='box')
 #plt.legend(loc="best")
 print(plot_data)
-sns.lmplot(x='Height', y='Lterl_Brnches', data=plot_data)
-y=plot_data['Lterl_Brnches']
+sns.lmplot(x='Height', y='Lateral_Branches', data=plot_data)
+y=plot_data['Lateral_Branches']
 X=sm.add_constant(plot_data['Height'])
 lm = sm.OLS(y, X).fit()
 print(lm.summary())
 
 ##Plot the data with histograms
-sns.jointplot(x='Height', y='Lterl_Brnches', data=plot_data, kind='reg')
-plt.show()
+sns.jointplot(x='Height', y='Lateral_Branches', data=plot_data, kind='reg')
+
 
 ##Poisson regression on lateral branches (count data)
-pois_reg = glm('Lterl_Brnches~Height', data=plot_data, family=sm.families.Poisson()).fit()
+pois_reg = glm('Lateral_Branches~Height', data=plot_data, family=sm.families.Poisson()).fit()
 print(pois_reg.summary())
+print("\nQuantities you can extract from lm:\n%s" % dir(pois_reg))
+print("\nCoefficients:\n%s" % pois_reg.params)
+print(pois_reg.predict())
+plot_data['PoissonPredictions'] = pd.Series(pois_reg.predict(), index=plot_data.index)
+
+sns.lmplot(x='Height', y='PoissonPredictions', data=plot_data)
+plt.show()
